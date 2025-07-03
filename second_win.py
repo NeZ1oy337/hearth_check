5fbff6a37e990ee0efde549fc229765ad7ba19a8
NeZ1oy337 <levangok12368@gmail.com>
2025-07-03 19:58:45 +0300

    addMain_window  interface

--- second_win.py
+++ second_win.py
@@ -1,1 +1,26 @@
-# напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QPushButton, QLabel, QLineEdit)
+
+from instr import *
+
+class Second_win(QWidget):
+    def __init__(self):
+        super()__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        pass
+    
+    def initUI(self):
+        pass
+
+    def connects(self):
+        pass
+
+    
